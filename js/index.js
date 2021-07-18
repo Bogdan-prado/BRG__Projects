31e37fe174bbc0b9f5442df9fc7d10469732d1b3
Bogdan <bgra4ov@gmail.com>
2021-07-18 18:24:27 +0300

    a few minor changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-let i = 0;
-window.onload = function () {
+/*
+function hide() {
   let preloader = document.getElementById('preloader');
   preloader.classList.add('hide-preloader');
   setInterval(function () {
     preloader.classList.add('preloader-hidden');
   }, 3000);
+}*/
+let i = 0;
+function move() {
   if (i == 0) {
     i = 1;
     var elem = document.getElementById("myProgress");
@@ -21,3 +24,5 @@ window.onload = function () {
     }
   }
 }
+/*window.addEventListener("load", hide);*/
+window.addEventListener("load", move);
