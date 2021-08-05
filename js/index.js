e6390ad43ee96a7ea3884327429622ae71d2f120
Bogdan <bgra4ov@gmail.com>
2021-08-05 06:37:10 +0300

    Full Screen Mobile menu

--- js/index.js
+++ js/index.js
@@ -80,8 +80,13 @@ function closeFullscreen() {
 /*hidden/side menu*/
   function toggleNav() {
   let nav = document.querySelector(".sidenav__main__block");
+    if(window.innerWidth >= 768){
     nav.style.width = window.getComputedStyle(nav).width === "0px" ? "calc(653px + (1632 - 653) * ((100vw - 768px) / (1920 - 768)))" : "0px";
+}  else {
+    nav.style.width = window.getComputedStyle(nav).width === "0px" ? "100vw" : "0px";
+} 
     let close = document.querySelector(".menu__title__opener");
     close.innerHTML = window.getComputedStyle(nav).width === "0px" ? "close" : "menu";
   }
 document.querySelector('.menu__title__opener').onclick = toggleNav;
+document.querySelector('.menu__title__blocks').onclick = toggleNav;
