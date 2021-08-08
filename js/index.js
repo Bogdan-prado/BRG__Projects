b0175805882452ddccba23454a24869fe98e52af
Bogdan <bgra4ov@gmail.com>
2021-08-08 19:41:01 +0300

    PC Hidden Menu Finished

--- js/index.js
+++ js/index.js
@@ -92,3 +92,22 @@ function closeFullscreen() {
   }
 document.querySelector('.menu__title__opener').onclick = toggleNav;
 document.querySelector('.menu__title__blocks').addEventListener("touchstart", toggleNav);
+
+
+/*TEST*/
+function openMenu(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
