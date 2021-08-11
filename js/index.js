735ab9e3e26b2f8cba5ac13e810e0f0f9f276c36
Bogdan <bgra4ov@gmail.com>
2021-08-11 09:16:20 +0300

    new

--- js/index.js
+++ js/index.js
@@ -95,7 +95,7 @@ document.querySelector('.menu__title__blocks').addEventListener("touchstart", to
 
 
 /*TEST*/
-function openMenu(evt, cityName) {
+function openMenu(evt, tabname) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -105,7 +105,7 @@ function openMenu(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabname).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
