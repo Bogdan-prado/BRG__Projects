dd5c12c58fc818e67bd5885f81b607df00282b54
Bogdan <bgra4ov@gmail.com>
2021-08-29 16:39:31 +0300

    New

--- js/jquery__index.js
+++ js/jquery__index.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
-    $("main").scroll(function () {
-        if ($("main").scrollTop() > 6.99) {
-            $(".main__center").slideUp(500, function () {
-                $(".test").slideDown(1000);
-            })
-        }
+ $(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).mouseWheel() > 0) {
+             $(".main__center").slideUp(1000, function () {
+            $(".test").slideDown(1000);
         })
+        }
+    })
 }) 
 
 
